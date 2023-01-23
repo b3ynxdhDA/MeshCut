a755ecd2afeda92bfcc00d9268cbd38b702c1a59
STD\s20217009 <20217009-YamadaRyuto@hcs.ac.jp>
2023-01-23 14:59:29 +0900

    プロジェクト立ち上げ、メッシュ切断とエディタ拡張のシーンを分けて、メッシュ切断からやる

---
+++ Assets/Scripts/MeshCut.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCut : MonoBehaviour
+{
+    private MeshFilter _meshFilter = default;
+
+    private Mesh _mesh = default; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
